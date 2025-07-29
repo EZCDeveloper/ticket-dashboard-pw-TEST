f88f15603e3397b900a08ce859e7809fc5a8dd88
Emanuel Zini Casaro <emanuelzinicasaro@Emanuels-MacBook-Pro.local>
2025-07-29 09:21:27 -0300

    direct tc01 developed

--- tests/regression/01_ticketHandling.test.ts
+++ tests/regression/01_ticketHandling.test.ts
@@ -1,8 +1,27 @@
 import { test } from '../../fixtures/base.fixtures';
+import { expect } from '@playwright/test';
+
 
 test.describe('1. CRUD Tickets', () => {
 
-   test('TC-1.1. Create a Ticket Successfully', async ({ basePage }) => {
+   test('TC-1.1. Create a Ticket Successfully', async ({ page, basePage }) => {
+      // 1. Arrange: Navigate to the home page
       await basePage.navigateTo("/")
+
+      // 2. Act: Create a ticket
+      await page.getByRole('link').filter({ hasText: /^$/ }).nth(1).click();
+      await page.getByTestId('title-input').click();
+      await page.getByTestId('title-input').fill('Develop new Application for Cats');
+      await page.getByTestId('description-textarea').click();
+      await page.getByTestId('description-textarea').fill('We need to develop new application');
+      await page.getByTestId('category-select').selectOption('Application Development');
+      await page.getByTestId('priority-3-radio').check();
+      await page.getByTestId('progress-range').fill('13');
+      await page.getByTestId('status-select').selectOption('started');
+      await page.getByTestId('status-select').click();
+      await page.getByTestId('submit-button').click();
+
+      // 3. Assert: Verify the ticket is created
+      await expect(page.getByText('Ticket created successfully')).toBeVisible();
    })
 })
